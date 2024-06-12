14a58ff069ffcbdf919072e1118abb40616d4c4c
MohitCode17 <mohit.codes17@gmail.com>
2024-06-12 23:21:38 +0530

    created tasks components

--- project-2 todo-app/src/components/Tasks/Tasks.jsx
+++ project-2 todo-app/src/components/Tasks/Tasks.jsx
@@ -1,7 +1,33 @@
+import Task from "../Task/Task";
 import styles from "./task.module.css";
 
-const Tasks = () => {
-  return <div>Tasks</div>;
+const Tasks = ({ tasks, toggleTaskComplete, handleDeleteTask }) => {
+  return (
+    <div className={styles.tasks}>
+      <header className={styles.header}>
+        <div>
+          <p>Created Tasks</p>
+          <span>5</span>
+        </div>
+
+        <div>
+          <p>Completed</p>
+          <span>2 of 5</span>
+        </div>
+      </header>
+
+      <div className={styles.tasksList}>
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            toggleTaskComplete={toggleTaskComplete}
+            handleDeleteTask={handleDeleteTask}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
