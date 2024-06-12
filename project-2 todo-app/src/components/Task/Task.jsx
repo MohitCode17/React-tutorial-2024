626a659119f646af735ea929f055be59c89fa913
MohitCode17 <mohit.codes17@gmail.com>
2024-06-12 23:30:29 +0530

    add task component

--- project-2 todo-app/src/components/Task/Task.jsx
+++ project-2 todo-app/src/components/Task/Task.jsx
@@ -1,5 +1,23 @@
-const Task = () => {
-  return <div>Task</div>;
+import styles from "./task.module.css";
+import { TbTrash } from "react-icons/tb";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
+const Task = ({ task }) => {
+  return (
+    <div className={styles.task}>
+      <button className={styles.checkContainer}>
+        {task.isComplete ? <BsFillCheckCircleFill /> : <div />}
+      </button>
+
+      <p className={task.isComplete ? styles.textCompleted : ""}>
+        {task.title}
+      </p>
+
+      <button className={styles.deleteButton}>
+        <TbTrash />
+      </button>
+    </div>
+  );
 };
 
 export default Task;
