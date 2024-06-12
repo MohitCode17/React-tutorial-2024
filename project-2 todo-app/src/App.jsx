acb970b39d20ea79128ad182be26a9c1d59d76b9
MohitCode17 <mohit.codes17@gmail.com>
2024-06-12 23:11:02 +0530

    add toggleComplete and delete functions

--- project-2 todo-app/src/App.jsx
+++ project-2 todo-app/src/App.jsx
@@ -18,10 +18,35 @@ const App = () => {
     ]);
   };
 
+  // TOGGLE COMPLETE
+  const toggleTaskComplete = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isComplete: !task.isComplete,
+        };
+      }
+      return task;
+    });
+
+    setTasks(newTasks);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+
+    setTasks(newTasks);
+  };
+
   return (
     <>
       <Header handleAddToTasks={handleAddToTasks} />
-      <Tasks tasks={tasks} />
+      <Tasks
+        tasks={tasks}
+        toggleTaskComplete={toggleTaskComplete}
+        handleDeleteTask={handleDeleteTask}
+      />
     </>
   );
 };
