3f1c7fa1aae3be52d6975e415bc840bfa1f615dc
MohitCode17 <mohit.codes17@gmail.com>
2024-06-13 17:05:00 +0530

    add toggle complete and delete task functionality

--- project-2 todo-app/src/components/Task/Task.jsx
+++ project-2 todo-app/src/components/Task/Task.jsx
@@ -2,10 +2,13 @@ import styles from "./task.module.css";
 import { TbTrash } from "react-icons/tb";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 
-const Task = ({ task }) => {
+const Task = ({ task, toggleTaskComplete, handleDeleteTask }) => {
   return (
     <div className={styles.task}>
-      <button className={styles.checkContainer}>
+      <button
+        className={styles.checkContainer}
+        onClick={() => toggleTaskComplete(task.id)}
+      >
         {task.isComplete ? <BsFillCheckCircleFill /> : <div />}
       </button>
 
@@ -13,7 +16,10 @@ const Task = ({ task }) => {
         {task.title}
       </p>
 
-      <button className={styles.deleteButton}>
+      <button
+        className={styles.deleteButton}
+        onClick={() => handleDeleteTask(task.id)}
+      >
         <TbTrash />
       </button>
     </div>
