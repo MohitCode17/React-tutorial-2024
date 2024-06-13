2cf5979e523801740db5388bca77393c93148936
MohitCode17 <mohit.codes17@gmail.com>
2024-06-13 18:14:39 +0530

    setting up routes and layout

--- project-3 github-user-search/src/main.jsx
+++ project-3 github-user-search/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./pages/Home.jsx";
+import User from "./pages/User.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="users/:userId" element={<User />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
