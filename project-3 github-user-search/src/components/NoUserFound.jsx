a58bf748d9d17948dbe81e9ad48d1e8f429e02fe
MohitCode17 <mohit.codes17@gmail.com>
2024-06-14 18:11:19 +0530

    add No user found ui

--- project-3 github-user-search/src/components/NoUserFound.jsx
+++ project-3 github-user-search/src/components/NoUserFound.jsx
@@ -1,16 +1,16 @@
+import { RxCrossCircled } from "react-icons/rx";
+import { Link } from "react-router-dom";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+
 const NoUserFound = () => {
   return (
-    <h1
-      style={{
-        fontFamily: "sans-serif",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: "100px",
-      }}
-    >
-      No user found with username
-    </h1>
+    <div className="user-not-found">
+      <RxCrossCircled size={30} />
+      <h3 className="user-not-found-title">User not found, Try Again</h3>
+      <Link className="user-not-found-navlink" to={"/"}>
+        <MdOutlineArrowBackIosNew size={30} /> Try Again
+      </Link>
+    </div>
   );
 };
 
